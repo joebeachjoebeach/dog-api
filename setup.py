014a020464bcae158162c8b89fff2cf3605d54ea
Joe Beach <joebeach@gmail.com>
2017-10-26 17:35:36 +0000

    update setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'dog_api',
+  packages = ['dog_api'],
+  version = '1.0',
+  description = 'A python wrapper for the Dog API from Dog CEO',
+  author = 'Joe Beach',
+  url = 'https://github.com/joebeachjoebeach/dog-api',
+  keywords = ['dogs'],
+  license = 'MIT'
+)
